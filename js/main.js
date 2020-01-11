88bc1e5130bff7128f1d2f728db510326a280dd9
pk-67 <pierre.kessler@gmail.com>
2020-01-11 18:38:14 +0100

    Corrections Texte

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2020/01/20';
+        var finalDate = '2020/01/27';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
